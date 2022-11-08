c9c1d8cdf87941348f5fe181d7d7bea69c130361
samuel <samuelwongjx.21@ichat.sp.edu.sg>
2022-11-08 11:41:15 +0800

    test

--- src/PiLedTest.py
+++ src/PiLedTest.py
@@ -1,13 +1,25 @@
-def init():
-    GPIO.setmode(GPIO.BCM) #choose BCM mode
-    GPIO.setwarnings(False)
-    GPIO.setup(22,GPIO.IN) #set GPIO 22 as input
+from hal import hal_input_switch as switch
+from hal import hal_led as led
+from PiDemo import blink_led
+from time import sleep
+import time
+
+def main():
+    led.init()
+    switch.init()
+    t_end = time.time() + 5
+    while True:
+
+        if switch.read_slide_switch():
+            t_end = time.time() + 5
+            blink_led(0.2)
+
+        else:
+            while time.time() < t_end:
+                blink_led(0.1)
+            led.set_output(0, 0)
 
 
-def read_slide_switch():
-    ret = 0
-
-    if GPIO.input(22):
-        ret = 1
-
-    return ret
+# Main entry point
+if __name__ == "__main__":
+    main()
